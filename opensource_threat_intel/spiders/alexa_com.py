f99717a25c51558d50962a366f0339fe370e51e8
Nanue1 <manue1@foxmail.com>
2017-07-20 00:30:11 +0800

    1500481810

--- opensource_threat_intel/spiders/alexa_com.py
+++ opensource_threat_intel/spiders/alexa_com.py
@@ -4,7 +4,6 @@
 import csv
 import os
 import time
-import urllib
 import zipfile
 
 from scrapy.spiders import CrawlSpider
@@ -18,7 +17,7 @@ class Spider(CrawlSpider):
     name = 'alexa.com'
 
     start_urls = [
-        'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip',
+        'http://www.alexa.com',
     ]
 
     # item数据格式规范
@@ -30,7 +29,7 @@ class Spider(CrawlSpider):
         item['data_type'] = 1
         item['tag'] = 11
         item['alive'] = True
-        #这里白名单的描述 为laexa排名
+        # 这里白名单的描述 为laexa排名
         item['description'] = line[0]
         item['confidence'] = 7
         item['source'] = 'alexa.com'
@@ -38,51 +37,30 @@ class Spider(CrawlSpider):
         item['created_time'] = now_time
         return item
 
-    # 判断数据源的tag类型
-    @staticmethod
-    def get_tag(tag):
-        if tag.find('3') >= 0:
-            return 7
-        elif tag.find('12') >= 0:
-            return 3
-        else:
-            return 0
 
-    def bak(self, response):
+    def parse_start_url(self, response):
+        download_url = ["http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"]
+        url = download_url[0]
         if not os.path.exists(DPATH):
             os.system('mkdir -p %s ' % DPATH)
-        filename = response.url.split("/")[-1]
+        filename = url.split("/")[-1]
         bak_file = '%s/%s%s' % (DPATH, time.strftime('%Y-%m-%d', time.localtime(time.time())), filename)
-        with open(bak_file, 'w') as f:
-            f.write(response.body)
-
-    def start_requests(self):
-        for url  in self.start_urls:
-            if not os.path.exists(DPATH):
-                os.system('mkdir -p %s ' % DPATH)
-            filename = url.split("/")[-1]
-            bak_file = '%s/%s%s' % (DPATH, time.strftime('%Y-%m-%d', time.localtime(time.time())), filename)
-            #当天下载过
-            if not os.path.exists(bak_file):
-                urllib.urlretrieve (url, bak_file)
-            else:
-                pass
-            #解压zip文件
-            bak_dir = '%s/%s' % (DPATH, time.strftime('%Y-%m-%d', time.localtime(time.time())))
-            # with zipfile.ZipFile(bak_file,"r") as zip_ref:
-            #     print bak_dir
-            #     zip_ref.extractall(bak_dir)
-            csv_file = bak_dir + '/' + 'top-1m.csv'
-            print  csv_file
-            self.parse_a(csv_file)
-    def parse(self, csv_file):
-        with open(csv_file,'rb') as f:
+        # 当天下载过
+        if not os.path.exists(bak_file):
+            cmd = " wget  -c '%s'  -O %s" % (url, bak_file)
+            os.system(cmd)
+        else:
+            pass
+        # 解压zip文件
+        bak_dir = '%s/%s' % (DPATH, time.strftime('%Y-%m-%d', time.localtime(time.time())))
+        with zipfile.ZipFile(bak_file, "r") as zip_ref:
+            print bak_dir
+            zip_ref.extractall(bak_dir)
+        csv_file = bak_dir + '/' + 'top-1m.csv'
+        print  csv_file
+        with open(csv_file, 'rb') as f:
             reader = csv.reader(f)
             for line in reader:
                 # print line
                 item = self.format_data(line)
                 yield item
-
-
-
-
---
+++ opensource_threat_intel/spiders/public_dns_info.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-   
+# Created by manue1 on 2017/7/19
+import time
+from scrapy.spiders import CrawlSpider
+from ..items import OpensourceThreatIntelItem
+DPATH = '../data_bak/public_dns_info'
+class Spider(CrawlSpider):
+    name = 'public-dns.info'
+    start_urls = [
+        'https://public-dns.info/nameservers-all.txt',
+    ]
+
+    # item数据格式规范
+    @staticmethod
+    def format_data(line):
+        item = OpensourceThreatIntelItem()
+        now_time = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(time.time()))
+        item['indicator'] = line[0]
+        item['data_type'] = 0
+        item['tag'] = 11
+        item['alive'] = True
+        item['description'] = '10000'
+        item['confidence'] = 5
+        item['source'] = 'public-dns.info'
+        item['updated_time'] = now_time
+        item['created_time'] = now_time
+        return item
+
+    def parse_start_url(self, response):
+        lines = response.body.strip().split('\n')
+        for line in lines:
+            print line
+            yield self.format_data(line)
