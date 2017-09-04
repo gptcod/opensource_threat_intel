2037871197a8772a4f82e4c33253b467ad69ddb3
李绪春 <lixuchun@intra.nsfocus.com>
2017-09-05 00:30:09 +0800

    1504542609

---
+++ opensource_threat_intel/spiders/spamhaus_org.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# from first.items import FirstItem
+import time
+
+from IPy import IP
+from scrapy import Request
+from scrapy.spiders import CrawlSpider
+
+from ..items import OpensourceThreatIntelItem
+
+
+class Spider(CrawlSpider):
+    name = "spamhaus.org"
+    allowed_domains = ["spamhaus.org"]
+    start_urls = [
+        'http://www.spamhaus.org/drop/drop.txt',
+        'http://www.spamhaus.org/drop/edrop.txt',
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url, callback=self.parse_1)
+
+    def parse_1(self, response):
+        tag = 3
+        data_type = 0
+        lines = response.body.split('\n')
+        for line in lines:
+            if not str.startswith(line, ';'):
+                if line:
+                    network = line.split(' ; ')[0]
+                    ipList = IP(network)
+                    for ip in ipList:
+                        item = OpensourceThreatIntelItem()
+                        now_time = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(time.time()))
+                        item['indicator'] = str(ip)
+                        item['data_type'] = data_type
+                        item['tag'] = tag
+                        item['alive'] = True
+                        item['description'] = 'none'
+                        item['confidence'] = 9
+                        item['source'] = 'spamhaus.org'
+                        item['updated_time'] = now_time
+                        item['created_time'] = now_time
+                        yield item
---
+++ opensource_threat_intel/spiders/vxvault_net.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import time
+
+from scrapy import Request
+from scrapy.spiders import CrawlSpider
+
+from ..items import OpensourceThreatIntelItem
+
+
+class Spider(CrawlSpider):
+    name = "vxvault.net"
+    allowed_domains = ["vxvault.net"]
+    start_urls = [
+        'http://vxvault.net/URL_List.php',
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url, callback=self.parse_1)
+
+    def parse_1(self, response):
+        tag = 7
+        data_type = 2
+        lines = response.body.split('\n')
+        for line in lines:
+            if str.startswith(line, 'http'):
+                if line:
+                    item = OpensourceThreatIntelItem()
+                    url = line.split(',')[0].strip()
+                    now_time = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(time.time()))
+                    item['indicator'] = url
+                    item['data_type'] = data_type
+                    item['tag'] = tag
+                    item['alive'] = False
+                    item['description'] = 'none'
+                    item['confidence'] = 10
+                    item['source'] = 'vxvault.net'
+                    item['updated_time'] = 'none'
+                    item['created_time'] = now_time
+                    yield item
